e25f8fa9683229153debf9d235c239ae00231b81
BaptouK <baptiste.dupuis2406@gmail.com>
2025-06-26 17:51:08 +0200

    :sparkles: Add collisions

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,27 @@
 std::mutex mutex;
 
 float lerp(float a, float b, float t) {
-    return a + t * (b - a);
+    return a+t*(b-a);
 }
 
 void update_ball(sf::Vector2f* p1,sf::Vector2f* p2,sf::Vector2f* coord_ball, sf::Vector2f* velocity,sf::CircleShape* ball) {
     std::lock_guard<std::mutex> lock(mutex);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    if (coord_ball->y >= 590 || coord_ball->y <= 0) {
+        velocity->y *= -1;
+    }
+    if (coord_ball->x <=10 && (coord_ball->y>=p1->y && coord_ball->y <= p2->y+180)) {
+        velocity->x *= -1;
+    }
+
+    if (coord_ball->x >=980 && (coord_ball->y>=p2->y && coord_ball->y <= p2->y+180)) {
+        velocity->x *= -1;
+    }
+
+    std::cout << p1->x << " " << p1->y << std::endl;
+    std::cout << p2->x << " " << p2->y << std::endl;
 
     float t = 0.1f;
     sf::Vector2f current = *coord_ball;
@@ -22,19 +36,25 @@ void update_ball(sf::Vector2f* p1,sf::Vector2f* p2,sf::Vector2f* coord_ball, sf:
     coord_ball->y = lerp(target.y, current.y, t);
 }
 
+void up_coord_ball(int &x,int &y, sf::Vector2f* coord_ball, sf::Vector2f* velocity) {
+    
+}
+
 int main() {
 
-    sf::RenderWindow window(sf::VideoMode(1000, 600), "Change the size");
+    int win_x = 1000;
+    int win_y = 600;
+
+    sf::RenderWindow window(sf::VideoMode(win_x, win_y), "Change the size");
     window.setFramerateLimit(60);
 
     int pixel_by_tick{10};
 
-    sf::RectangleShape background(sf::Vector2f(1000, 600));
+    sf::RectangleShape background(sf::Vector2f(win_x, win_y));
     background.setPosition(sf::Vector2f(0, 0));
     background.setFillColor(sf::Color::Black);
 
     window.draw(background);
-
 
     sf::Vector2f coord_player1(1.f, 0.f);
     sf::RectangleShape player1(sf::Vector2f(15, 180));
@@ -50,11 +70,8 @@ int main() {
     sf::Vector2f velocity (sf::Vector2f(10,10));
     ball.setFillColor(sf::Color::Green);
 
-
-
     while (window.isOpen()) {
         sf::Event event;
-
 
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -72,9 +89,9 @@ int main() {
 
         window.clear();
 
-
         player1.setPosition(coord_player1);
         player2.setPosition(coord_player2);
+
         ball.setPosition(coord_ball);
 
         window.draw(player1);
